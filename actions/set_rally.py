97edadfc60a98a924ab41bb4d0c0c239a36c6c02
FredNoonienSingh <58219072+FredNoonienSingh@users.noreply.github.com>
2023-06-25 03:39:14 +0200

    written a nicer way to use abliities

--- actions/set_rally.py
+++ actions/set_rally.py
@@ -8,3 +8,6 @@ from sc2.position import Point3
 
 async def set_rally(bot:BotAI, unit:Unit, target:Union[Point3, Unit]):
     bot.do(unit(AbilityId.RALLY_BUILDING,target))
+
+async def set_nexus_rally(bot:BotAI, nexus:Unit, target:Union[Point3, Unit]):
+    bot.do(nexus(AbilityId.RALLY_NEXUS, target))
