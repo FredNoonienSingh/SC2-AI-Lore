dac52654d5c851877a5e2a9793ffae40bd1488c9
Frederic <58219072+FredNoonienSingh@users.noreply.github.com>
2023-07-04 15:52:24 +0200

    Revert "steps towards build order as list"

--- actions/build_structure.py
+++ actions/build_structure.py
@@ -1,27 +1,13 @@
-from typing import Union
-
-"""sc2"""
+from sc2.bot_ai import BotAI
 from sc2.unit import Unit
 from sc2.units import Units
-from sc2.bot_ai import BotAI
-from sc2.position import Point3
 from sc2.ids.unit_typeid import UnitTypeId
 
-"""utils"""
-from util.can_build import can_build_structure
 
-async def build_gas(self : BotAI, nexus) -> bool:
+async def build_gas(self : BotAI, nexus):
     vespene: Units = self.vespene_geyser.closer_than(12, nexus)[0]
     if await self.can_place_single(UnitTypeId.REFINERY, vespene.position):
         workers: Units = self.workers.gathering
         if workers:
             worker: Unit = workers.closest_to(vespene)
             worker.build_gas(vespene)
-            return True
-    return False
-
-async def build_structure(bot:BotAI, structure:UnitTypeId, build_pos:Union[Point3, Unit], worker) -> bool:
-    if can_build_structure(bot, structure):
-        await bot.build(structure,near=build_pos,build_worker=worker)
-        return True
-    return False
--- util/can_build.py
+++ util/can_build.py
@@ -4,12 +4,12 @@ from sc2.ids.unit_typeid import UnitTypeId
 
 
 def can_build_structure(bot:BotAI, structure_id)-> bool:
-    return bot.can_afford(structure_id) and bot.tech_requirement_progress(structure_id)
+    return bot.can_afford(structure_id) and bot.tech_requirement_progress(structure_id) and not bot.already_pending(structure_id)
 
 def can_build_unit(bot:BotAI, unit_id, probe_limit:int=66) ->bool:
     if unit_id == UnitTypeId.PROBE and len(bot.units(unit_id)) > probe_limit:
         return False
-    return bot.can_afford(unit_id) and bot.can_feed(unit_id) and bot.tech_requirement_progress(unit_id)
+    return bot.can_afford(unit_id) and bot.can_feed(unit_id) and bot.tech_requirement_progress(unit_id) and not bot.already_pending(unit_id)
 
 def can_research_upgrade(bot:BotAI,upgrade_id)->bool:
     return bot.can_afford(upgrade_id) and not bot.already_pending_upgrade(upgrade_id) and bot.tech_requirement_progress(upgrade_id)
